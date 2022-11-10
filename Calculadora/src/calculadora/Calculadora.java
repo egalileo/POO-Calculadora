c54a39f59d01603c47efb0172939cb16902ca784
Gali Padilla <erick.galileo@outlook.com>
2022-11-10 17:48:12 -0600

    ejercicio resuelto

--- Calculadora/src/calculadora/Calculadora.java
+++ Calculadora/src/calculadora/Calculadora.java
@@ -4,13 +4,24 @@ package calculadora;
 import javax.swing.JOptionPane;
 
 /**
- *
  * @author Galileo Padilla
  */
 public class Calculadora {
-
     public static void main(String[] args) {
-
+        int numero, base;
+        Conversor calculoVoid = new Conversor(); //Crear mi objeto con constructor vacio
+        
+        
+        numero = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero que desea convertir ")); 
+        base = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la base a la que desea convertir (Binario, Octal o Hex)")); 
+        
+        Conversor calculoConstructor = new Conversor( numero, base ); //Crear mi objeto con constructor 
+        
+        calculoVoid.convertirVoid(numero, base);
+        calculoConstructor.convertirReturn();
+        
+        
+        
     }
     
 }
--- Calculadora/src/calculadora/Conversor.java
+++ Calculadora/src/calculadora/Conversor.java
@@ -4,5 +4,44 @@ import javax.swing.JOptionPane;
 
 public class Conversor {
 
-   
+    public int base, numero, temporal, residuo;
+    public String caracteres = "0123456789ABCDEF", salida ="";
+
+    public Conversor() {
+    }
+    
+    public Conversor(int numero, int base){
+        this.numero = numero;
+        this.base = base;
+    }
+    
+    public String convertirReturn(){
+        this.salida="";
+        this.temporal = this.numero;
+
+        while (temporal > 0) {
+            this.residuo = this.temporal % this.base;
+            this.temporal = this.temporal / this.base;
+            this.salida = caracteres.charAt(this.residuo) + this.salida;
+        }
+        JOptionPane.showMessageDialog(null, "El numero: " + this.numero
+                + " en Decimal\nconvertido a base"+this.base+" es: " + this.salida);
+        salida = "";
+        return this.salida;
+    }
+    
+    
+    public void convertirVoid(int numero, int base) {
+        this.salida="";
+        this.temporal = numero;
+
+        while (temporal > 0) {
+            residuo = this.temporal % base;
+            this.temporal = this.temporal / base;
+            this.salida = caracteres.charAt(this.residuo) + this.salida;
+        }
+        JOptionPane.showMessageDialog(null, "El numero: " + numero
+                + " en Decimal\nconvertido a base"+base+" es: " + this.salida);
+        salida = "";
+    }
 }
