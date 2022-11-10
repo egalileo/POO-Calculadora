ad1bff029b704aa4f6e456f70f11157d89c3a1f9
Gali Padilla <erick.galileo@outlook.com>
2022-11-10 17:22:37 -0600

    reinicio del ejercicio

--- Calculadora/src/calculadora/Calculadora.java
+++ Calculadora/src/calculadora/Calculadora.java
@@ -10,18 +10,7 @@ import javax.swing.JOptionPane;
 public class Calculadora {
 
     public static void main(String[] args) {
-        int numero, base;
-        String salida;
-        
-        numero = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el  numero que desea convertir"));
-        base = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la base a la que desea convertir"));
-        
-        Conversor calcu = new Conversor(numero, base);
-        calcu.convertir();
-        
-        Conversor calcu2 = new Conversor();
-        salida = calcu2.convertir2(numero, base);
-        JOptionPane.showMessageDialog(null,"El numero "+numero+"\n covertido a base "+base+" es:"+salida);
+
     }
     
 }
--- Calculadora/src/calculadora/Conversor.java
+++ Calculadora/src/calculadora/Conversor.java
@@ -4,38 +4,5 @@ import javax.swing.JOptionPane;
 
 public class Conversor {
 
-    public int numero, base, temporal, residuo;
-    public String caracteres = "0123456789ABCDEF", salida="";
-
-    public Conversor() {
-    }
-
-    public Conversor(int numero, int base) {
-        this.numero = numero;
-        this.base = base;
-    }
-
-    public void convertir() {
-        salida = "";
-        temporal = this.numero;
-        while (temporal > 0) {
-            residuo = temporal % this.base;
-            temporal = temporal / this.base;
-            salida = caracteres.charAt(residuo) + salida;
-        }
-        JOptionPane.showMessageDialog(null, "El numero: " + this.numero
-                + " en Decimal\nconvertido a base "+this.base+" es: " + salida);
-        
-    }
-    
-    public String convertir2(int numero, int base){
-        salida = "";
-        temporal = numero;
-        while (temporal > 0) {
-            residuo = temporal % base;
-            temporal = temporal / base;
-            salida = caracteres.charAt(residuo) + salida;
-        }
-        return salida;
-    }
+   
 }
